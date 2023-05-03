76c349ecc24f1b6c03e78b937fb249e910274da5
shephali09 <hiremathshephali9@gmail.com>
2023-05-04 00:45:29 +0530

    Add: Ability to insert new Contacts to Address Book

--- AddressBookSystemDB/AddressBookSystem.sql
+++ AddressBookSystemDB/AddressBookSystem.sql
@@ -10,3 +10,16 @@ state varchar(50) not null, zip int not null,  phone_number long not null,
 email varchar(50) not null, primary key(id)); 
 
 describe address_book;
+
+---- UC3 ----
+insert into address_book(first_name,last_name,address,city,state,zip,phone_number,email) values 
+('Shephali','Hiremath','JM Road','Kolhapur','Maharashtra',416115,8329596787,'shephali@gmail.com');
+
+insert into address_book(first_name,last_name,address,city,state,zip,phone_number,email) values 
+('Harshad','Sharma','MG Chowk','Pune','Maharashtra',412365,9595030156,'harshad2@gmail.com');
+
+insert into address_book(first_name,last_name,address,city,state,zip,phone_number,email) values 
+('Aditi','Patil','JK Nagar','Panjim','Goa',432567,7772014569,'aditi@gmail.com');
+
+insert into address_book(first_name,last_name,address,city,state,zip,phone_number,email) values 
+('Vidya','Swami','Shahapur','Banaras','UP',145789,7418523202,'vidya3@gmail.com');
