d3151c39b35ded066ae389dcd6d292b6f278fbd7
shephali09 <hiremathshephali9@gmail.com>
2023-05-04 00:39:46 +0530

    Add: Ability to create a Address Book Table with first and last names, address, city, state, zip, phone number and email as its attributes

--- AddressBookSystemDB/AddressBookSystem.sql
+++ AddressBookSystemDB/AddressBookSystem.sql
@@ -3,8 +3,10 @@ show databases;
 create database address_book_service;
 use address_book_service;
 
+---- UC2 ----
+create table address_book(id int not null auto_increment, first_name varchar(50) not null, 
+last_name varchar(50) not null, address varchar(200) not null, city varchar(50) not null, 
+state varchar(50) not null, zip int not null,  phone_number long not null, 
+email varchar(50) not null, primary key(id)); 
 
-
-
-
-
+describe address_book;
